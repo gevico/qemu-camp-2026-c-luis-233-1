3f0b11777e8c6b33a4d5edac995e7c1429868aaa
luis233 <202521070123@std.uestc.edu.cn>
2026-04-14 16:47:52 +0800

    完成第16题

--- exercises/16_mysed/16_mysed.c
+++ exercises/16_mysed/16_mysed.c
@@ -9,15 +9,38 @@ int parse_replace_command(const char* cmd, char** old_str, char** new_str) {
         return -1;
     }
 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    const char *p = cmd + 2;
+    const char *sep1 = strchr(p, '/');
+    if (!sep1) return -1;
+
+    const char *sep2 = strchr(sep1 + 1, '/');
+    if (!sep2) return -1;
+
+    int old_len = sep1 - p;
+    int new_len = sep2 - sep1 - 1;
+
+    *old_str = (char *)malloc(old_len + 1);
+    *new_str = (char *)malloc(new_len + 1);
+    if (!*old_str || !*new_str) return -1;
+
+    strncpy(*old_str, p, old_len);
+    (*old_str)[old_len] = '\0';
+    strncpy(*new_str, sep1 + 1, new_len);
+    (*new_str)[new_len] = '\0';
 
     return 0;
 }
 
 void replace_first_occurrence(char* str, const char* old, const char* new) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    char *pos = strstr(str, old);
+    if (!pos) return;
+
+    int old_len = strlen(old);
+    int new_len = strlen(new);
+    int tail_len = strlen(pos + old_len) + 1;
+
+    memmove(pos + new_len, pos + old_len, tail_len);
+    memcpy(pos, new, new_len);
 }
 
 int main(int argc, char* argv[]) {
